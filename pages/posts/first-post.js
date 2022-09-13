7f3fa8931d69a716b71dc07cda272078d7fe8b8b
thelovesmith <averydante9@gmail.com>
2022-09-13 00:22:13 -0400

    Finished Css lesson up to layout component and Css Modules

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../../components/layout";
 function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <h1> First Post</h1>
       <h2>
         <Link href="/">Back to Home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
 export default FirstPost;
